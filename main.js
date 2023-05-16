99a01a38a09deb067b3df104f790678e4ecf80ad
sabbir <mdsabbirrahmanm740@gmail.com>
2023-05-16 21:28:44 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+const comment=[
+    {
+        postId: 1,
+        id: 1,
+        name: "id labore ex et quam laborum",
+        email: "Eliseo@gardner.biz",
+        body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+      },
+      {
+        postId: 1,
+        id: 2,
+        name: "quo vero reiciendis velit similique earum",
+        email: "Jayne_Kuhic@sydney.com",
+        body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+      },
+      {
+        postId: 1,
+        id: 3,
+        name: "odio adipisci rerum aut animi",
+        email: "Nikita@garfield.biz",
+        body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+      },
+]
+
+const user=[
+    {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+          zipcode: "92998-3874",
+          geo: {
+            lat: "-37.3159",
+            lng: "81.1496"
+          }
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+          name: "Romaguera-Crona",
+          catchPhrase: "Multi-layered client-server neural-net",
+          bs: "harness real-time e-markets"
+        }
+      },
+
+
+      {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@melissa.tv",
+        address: {
+          street: "Victor Plains",
+          suite: "Suite 879",
+          city: "Wisokyburgh",
+          zipcode: "90566-7771",
+          geo: {
+            lat: "-43.9509",
+            lng: "-34.4618"
+          }
+        },
+        phone: "010-692-6593 x09125",
+        website: "anastasia.net",
+        company: {
+          name: "Deckow-Crist",
+          catchPhrase: "Proactive didactic contingency",
+          bs: "synergize scalable supply-chains"
+        }
+      },
+];
+const id=1;
+const findUser=user.find(user=>user.id===id);
+// console.log(findUser);
+
+const findComment=comment.filter(comment=> comment.name.length);
+console.log(findComment)
+
